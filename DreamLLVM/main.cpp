a881f798e716bc94775096c3539d4f79cf5c4f2a
Marc <mfervil1@depaul.edu>
2021-02-07 20:55:47 -0600

    Initial Commit

---
+++ DreamLLVM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DreamLLVM
+//
+//  Created by Marc Fervil on 2/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
